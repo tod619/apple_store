7cee96cf6109562efdddb324871c7cd1224e8d27
tod619 <trevorodwyer619@hotmail.com>
2021-01-19 15:13:57 +0000

    create function to changes slides in slideshow infinetly in script.js file

--- script.js
+++ script.js
@@ -5,9 +5,38 @@ const slideshowDivs = () => {
 
         div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`;
 
+        // add class change to first div
+        i === 1 && div.classList.add('change')
+
         document.querySelector('.slideshow').appendChild(div);
     }
 }
 
 slideshowDivs()
+
+const divs = document.querySelectorAll('.slideshow div')
+
+let a = 1;
+
+const slideshow = () => {
+    setInterval(() => {
+
+        a++;
+
+        const div = document.querySelector('.slideshow .change')
+
+        div.classList.remove('change')
+
+        if(a < divs.length) {
+            div.nextElementSibling.classList.add('change');
+        } else {
+            divs[0].classList.add('change');
+            a = 1;
+        }
+
+        
+    },20000)
+}
+
+slideshow()
 // End: Slideshow
