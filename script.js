0a4ef84060ffd071c50e696738c277c41a3ee0ff
tod619 <trevorodwyer619@hotmail.com>
2021-01-21 15:03:02 +0000

    add function to rotate cube along z axies

--- script.js
+++ script.js
@@ -2,26 +2,31 @@
 const cube = document.querySelector('.cube');
 let y = 0
 let x = 0
+let z = 0
 
 // iPhone Cube arrow controls
-document.querySelector('.top-x-control').addEventListener('click',
-()=>{
-    cube.style.transform = `rotateX(${x+=20}deg)`
+document.querySelector('.top-x-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x += 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`
 })
 
-document.querySelector('.bottom-x-control').addEventListener('click',
-()=>{
-    cube.style.transform = `rotateX(${x-=20}deg)`
+document.querySelector('.bottom-x-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x -= 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`
 })
 
-document.querySelector('.left-y-control').addEventListener('click',
-()=>{
-    cube.style.transform = `rotateY(${y+=20}deg)`
+document.querySelector('.left-y-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y -= 20}deg) rotateZ(${z}deg) `
 })
 
-document.querySelector('.right-y-control').addEventListener('click',
-()=>{
-    cube.style.transform = `rotateY(${x-=20}deg)`
+document.querySelector('.right-y-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y += 20}deg) rotateZ(${z}deg) `
+})
+
+document.querySelector('.top-z-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z -= 20}deg) `
+})
+
+document.querySelector('.bottom-z-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z += 20}deg) `
 })
 
 // Make Cube rotate on the Y axis
